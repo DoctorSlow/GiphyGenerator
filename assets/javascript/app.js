dcfa5f53827e11cc8d65141d63745e8d4f769b38
Jeremy J. Lee <jeremyjlee@Jeremys-MacBook-Pro-2.local>
2018-03-04 23:28:19 -0700

    added some functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,52 @@
-  //event listener for button
-  $("#giphy").on("click", function () {
+window.onload = function () {
 
-      //giphy API URL for image searches
-      var queryURL =
-          "https://api.giphy.com/v1/gifs/random?api_key=nCwoop3mIMx3QrpKYzYtYjHlWiFDYgmJ&tag=cats&limit=10";
+    $(document).ready(function () {
 
-      //AJAX GET request via the queryURL
-      $.ajax({
-              url: queryURL,
-              method: "GET"
-          })
-          //wait until AJAX request comes back
-          .then(function (response) {
+        var topics = ["cats", "hotdogs", "dogs", "bananas"];
+        var gifs;
 
-              //save the image_original_url property
-              var imageURL = response.data.image_original_url;
+        function displayGifs() {
+            $("#themes-view").empty();
+            var theme = $(this).attr("data-name");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=nCwoop3mIMx3QrpKYzYtYjHlWiFDYgmJ&q=" + theme + "&limit=10&offset=0&lang=en";
 
-              //create and store image tag
-              var catImage = $("<img>");
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                for (var i = 0; i < response.data.length; i++) {
+                    var wrap = $("<div>").addClass("gif-wrap col-md-4");
+                    var rating = $("<p>").text(response.data[i].rating).addClass("rating-value");
+                    var gif = $("<img>").attr("src", response.data[i].images.url);
+                    gif.attr("alt", term + " img");
+                    gif.addClass("gif-img");
+                    wrap.append(gif, rating);
+                    $("#themes-view").append(wrap);
+                }
+            });
+        }
 
-              //set the catImage src and attribute to imageURL
-              catImage.attr("src", imageURL);
-              catImage.attr("alt", "cat image");
+        function renderButtons() {
+            $("#buttons-view").empty();
+            for (var i = 0; i < topics.length; i++) {
+                var a = $("<button>");
+                a.addClass("theme-btn");
+                a.attr("data-name", topics[i]);
+                a.text(topics[i]);
+                $("#buttons-view").append(a);
+            }
+        }
 
-              //prepend catImage to the images div
-              $("#images").prepend(catImage);
-          });
-  });
+        $("#add-theme").on("click", function (event) {
+            event.preventDefault();
+            var theme = $("#theme-input").val().trim();
+            topics.push(theme);
+            renderButtons();
+        });
+
+        $(document).on("click", ".theme-btn", displayGifs);
+        renderButtons();
+        displayGifs();
+    });
+}
